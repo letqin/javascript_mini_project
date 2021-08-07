50252d950fcbc9249c1b5eb7f64f4497e240c5d0
letqin <letqin@gmail.com>
2021-08-07 14:28:46 -0400

    added more javascript for ties and more pseudo code

--- script_two.js
+++ script_two.js
@@ -8,11 +8,15 @@ var userChoice = prompt("Choose R, P, or S")
 
 // computer makes a choice
 var choices = ["R", "P", "S"]
-var computerChoice = choices[]
+var computerChoice = choices[Math.floor( Math.random() * choices.length )]
 
 // show computer choice to user
 alert("Computer chose " + computerChoice)
 
 // check for tie
+if (userChoice === computerChoice) {
+    ties += ties // adds one on the value of ties
+}
+
 // it's a tie
 // increment ties
