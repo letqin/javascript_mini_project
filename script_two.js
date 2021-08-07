dd15a4eaede9d42fd9c5b67e2eda87567e4c0569
letqin <86627655+letqin@users.noreply.github.com>
2021-08-07 14:13:40 -0400

    going mobile

---
+++ script_two.js
@@ -0,0 +1,10 @@
+// variables for wins, losses, and ties
+var wins = 0;
+var loss = 0;
+var ties = 0;
+
+// prompt user to choose r, p, s
+var wantsToPlayAgain = confirm()
+
+// computer makes a choice
+// show computer choice to user
