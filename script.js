46afb8457fc451029989cdabd5d6e456c78795d2
letqin <86627655+letqin@users.noreply.github.com>
2021-08-07 13:54:15 -0400

    added javascript and html files

---
+++ script.js
@@ -0,0 +1,29 @@
+// declaring variables
+let wins = 0
+let losses = 0
+let ties = 0
+let userSelection =""
+// cpu picker math
+function cpuPick() {
+    var x =Math.floor(Math.random() * cpuSelection.length)
+    alert("I choose " + cpuSelection[x] + "!")
+}
+
+function userPrompt () { //prompts user for choice
+    var getUserSelect = window.prompt("Enter r, p, or s: ");
+    if (getUserSelect != "r" && getUserSelect != "p" && getUserSelect != "s" ) {
+        userSelection = "Not a valid entry"
+    } else {userSelection = getUserSelect  }  
+    console.log(userSelection)  
+}
+
+var playerChoice = prompt( "R", "S", "P");
+var computerChoice = Math.random();
+if (computerChoice < 0.31) {
+    computerChoice ="R";
+}
+ else if(computerChoice <=0.59) {
+    computerChoice ="S";
+ } //prints computer choice
+ else {computerChoice ="P";
+ window.alert("The computer chose " + computerChoice)
